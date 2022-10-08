3ddbf99e4bea859a21f5cb9eabdc68ecd5b84eb2
Suryanshu <tshuryanshu@gmail.com>
2022-10-08 10:27:34 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container, AppBar, Typography } from '@material-ui/core';
+//import { Container } from '@material-ui/core';
+//import Posts from "./components/Posts/Posts";
+//import Form from "./components/Form/Form";
+import memories from './images/memories.png';
+
+const App = () => {
+    return (
+         <Container fixed={true}>
+            <AppBar position="static" color="inherit">
+                <Typography align="center" variant="h2">Shubham</Typography>
+                <img src={memories} ></img>
+            </AppBar>
+        </Container>
+
+    );
+    }
+
+export default App;
